eff7c3557f3d0509933389a72dbceb2b95332cb8
EverGreenCoin <EverGreenCoinDev@gmail.com>
2019-11-27 18:04:00 -0500

    High DPI
    QGuiApplication

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -84,12 +84,13 @@ BitcoinGUI::BitcoinGUI(QWidget *parent):
     rpcConsole(0)
 {
 
-#if QT_VERSION >= QT_VERSION_CHECK(5,6,0)
-    // QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
-    QApplication::setAttribute(Qt::AA_DisableHighDpiScaling);
-#else
-    qputenv("QT_DEVICE_PIXEL_RATIO", QByteArray("1"));
-#endif // QT_VERSION
+#if QT_VERSION > 0x050100
+    // Generate high-dpi pixmaps
+    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+#endif
+#if QT_VERSION >= 0x050600
+    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
 
     resize(1000, 700);
     setWindowTitle(tr("EverGreenCoin® Core - Wallet v1.9") );
