e47ed4d6701a14188831bc49c0a0e07e01746208
EverGreenCoin <EverGreenCoinDev@gmail.com>
2024-12-20 19:56:57 -0500

    Update walletdb.cpp

--- src/walletdb.cpp
+++ src/walletdb.cpp
@@ -741,7 +741,7 @@ bool BackupWallet(const CWallet& wallet, const string& strDest)
 
                 try {
 #if BOOST_VERSION >= 104000
-                    boost::filesystem::copy_file(pathSrc, pathDest, filesystem::copy_option::overwrite_if_exists);
+                    boost::filesystem::copy_file(pathSrc, pathDest, boost::filesystem::copy_option::overwrite_if_exists);
 #else
                     boost::filesystem::copy_file(pathSrc, pathDest);
 #endif
